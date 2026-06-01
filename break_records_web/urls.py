7928e786fcc9a4fd7d7f60ca9499931efc4a8abd
Aragorn3022 <aswadyousaf9@gmail.com>
2026-06-01 21:11:29 +0500

    Add redirect from root URL to API endpoint

--- break_records_web/urls.py
+++ break_records_web/urls.py
@@ -1,7 +1,9 @@
 """URL configuration for Break Records PoC."""
 
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path("", lambda request: redirect("/api")),
     path("api/", include("agent.urls")),
 ]
